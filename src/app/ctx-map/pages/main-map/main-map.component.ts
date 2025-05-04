275ebffe2b11661670aebc9e2369271f778c8d34
t-dlsantos <thiagodlsantos9@gmail.com>
2025-05-04 20:41:10 -0300

    feat: map integration with api

--- src/app/ctx-map/pages/main-map/main-map.component.ts
+++ src/app/ctx-map/pages/main-map/main-map.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { BaseAbstract } from '../../../libraries/abstracts';
 import { MessageService } from 'primeng/api';
 import { LoadingService } from '../../../ctx-layout/layout/service/loading.service';
 import { WaterLevelStatus } from '../../enums';
+import { Device } from '../../../api/devices';
+import { DevicesService } from '../../../api/devices';
+
+import * as L from 'leaflet';
 
 @Component({
     selector: 'app-main-map',
@@ -10,22 +14,74 @@ import { WaterLevelStatus } from '../../enums';
     styleUrls: ['./main-map.component.scss'],
     standalone: false
 })
-export class MainMapComponent extends BaseAbstract {
-    devices: any[] = mockData;
-    selectedDevice: any = mockData[0];
-
-    constructor(messageService: MessageService, loadingService: LoadingService) {
+export class MainMapComponent extends BaseAbstract implements OnInit {
+    devices: Device[] = [];
+    selectedDevice: Device | null = null;
+    mapLayers: L.Layer[] = [];
+    
+    constructor(
+        messageService: MessageService, 
+        loadingService: LoadingService, 
+        private devicesService: DevicesService,
+        private ngZone: NgZone
+    ) {
         super(messageService, loadingService);
     }
-}
 
-const mockData = [
-    {
-        id: 1,
-        name: 'Estação 1',
-        latitude: -23.5505,
-        longitude: -47.60013,
-        address: 'Rua Teste, 123 | Sorocaba/SP',
-        waterLevelStatus: WaterLevelStatus.Normal
+    ngOnInit() {
+        this.loadDevices();
     }
-];
+
+    loadDevices() {
+        this.loadingService.start();
+        this.devicesService.obterTodos().subscribe({
+            next: (devices) => {
+                this.devices = devices;
+                this.createMapLayers();
+                this.loadingService.stop();
+            },
+            error: (error) => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Erro',
+                    detail: 'Erro ao carregar dispositivos'
+                });
+                this.loadingService.stop();
+            }
+        });
+    }
+
+    createMapLayers() {
+        this.mapLayers = this.devices.map(device => {
+            const marker = L.marker([device.latitude, device.longitude])
+                .bindPopup(device.name)
+                .setIcon(this.getMarkerIcon(device));
+            
+            marker.on('click', () => {
+                this.ngZone.run(() => {
+                    this.selectedDevice = device;
+                });
+            });
+
+            marker.on('popupclose', () => {
+                this.ngZone.run(() => {
+                  this.selectedDevice = null;
+                });
+            });
+            
+            return marker;
+        });
+    }
+
+    getMarkerIcon(device: any) {
+        const baseIcon = L.icon({
+            iconUrl: '/assets/images/marker-icon.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        return baseIcon;
+    }
+}
